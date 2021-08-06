8d18b8c5a770cb79283c21645c5c31f916c871c2
Isaac A. Gonzalez <isaaca.gonzalez228@gmail.com>
2021-08-06 10:17:20 -0700

    commented out class based component. replaced with function based component

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-class Hello extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello there!</h1>
-        <h1>Hello there!</h1>
-        <h1>Hello there!</h1>
-      </div>
-    );
-  }
+// class Hello extends React.Component {
+// render() {
+//   // only one item can be returned so in order to show multiple things you have to wrap it in a single div like below.
+//   return (
+//     <div>
+//       <h1>Hello there!</h1>
+//       <h1>Hello there!</h1>
+//       <h1>Hello there!</h1>
+//     </div>
+//   );
+// }
+// }
+
+function Hello() {
+  return (
+    <div>
+      <h1>Hello there!</h1>
+      <h1>Hello there!</h1>
+      <h1>Hello there!</h1>
+    </div>
+  );
 }
 
 ReactDOM.render(<Hello />, document.getElementById("root"));
